9946cf1bac398632d87888e0d83d39b769bdfd9c
Isaac Park <isaac.a.park@gmail.com>
2016-02-07 00:11:43 -0500

    Add Shutdown Message

--- PiShutdown.py
+++ PiShutdown.py
@@ -6,5 +6,6 @@ GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-GPIO.wait_for_edge(24, GPIO.RISING)
+GPIO.wait_for_edge(13, GPIO.RISING)
+print("Shutting down, NOW!")
 os.system("poweroff")
