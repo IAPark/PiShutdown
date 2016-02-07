c2097122a0bac7bb9bb1ab780ae0377054c4e8f2
Isaac Park <isaac.a.park@gmail.com>
2016-02-07 00:23:22 -0500

    Timing Bug Fix

--- PiShutdown.py
+++ PiShutdown.py
@@ -1,11 +1,14 @@
-
 import RPi.GPIO as GPIO
 import os
+import time
 GPIO.setmode(GPIO.BCM)
 
 
 GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+time.sleep(0.5)
+
 GPIO.wait_for_edge(13, GPIO.RISING)
 print("Shutting down, NOW!")
 os.system("shutdown -h now")
+GPIO.cleanup()
